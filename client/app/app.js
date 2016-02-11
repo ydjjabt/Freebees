86de9c0e4b12b8808b87b2a8217e13142244bf25
alicekao <alicekao.ak@gmail.com>
2016-02-11 12:33:48 -0800

    Created dummy data arr and successfully looped through to place multiple markers on map

--- client/app/app.js
+++ client/app/app.js
@@ -5,33 +5,50 @@ var initMap = function(){
     zoom: 14
   });
 
-  // var geocoder = new google.mapsGeocoder();
+  var geocoder = new google.maps.Geocoder();
+  //test data array
+  var test = [
+    {
+      myLatLng: {lat: 37.7833, lng: -122.4167},
+      contentString: "Goodbye World"
+    },
+    {
+      myLatLng: {lat: 38.7833, lng: -122.4167},
+      contentString: "Goodbye World #2"
+    }
+  ];
 
-  var myLatLng = {lat: 37.7833, lng: -122.4167};
-  var contentString = "Goodbye World";
+  //loop through data returned from db to place on map
+  for (var i = 0; i < test.length; i++) {
+    var infowindow = new google.maps.InfoWindow({
+        content: test[i].contentString
+      });
 
-  var infowindow = new google.maps.InfoWindow({
-      content: contentString
-    });
+    var marker = new google.maps.Marker({
+        position: test[i].myLatLng,
+        map: map,
+        title: 'Hello World!'
+      });
 
-  var marker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      title: 'Hello World!'
-    });
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
+  }
+  //geocode inputted address on submit
+  document.getElementById('submit').addEventListener('click', function(){
+    geocodeAddress(geocoder, map);
+  });
+};
 
-  marker.addListener('click', function() {
-      infowindow.open(map, marker);
-    });
-
-  // document.getElementById('submit').addEventListener('click', function(){
-  //   geocodeAddress(geocoder, map);
-  // });
-}
-
-// var geocodeAddress = function(geocoder, resultsMap){
-//   var address = document.getElementById('address').value;
-//   geocoder.geocode({'address': address}, function(results, status) {
-//     console.log('trying to geocode');
-//   });
-// }
+var geocodeAddress = function(geocoder, resultsMap){
+  var address = document.getElementById('address').value;
+  alert('address is, ', address);
+  geocoder.geocode({'address': address}, function(results, status) {
+    console.log('results from geocodeaddress fn is: ', results);
+    if (status == google.maps.GeocoderStatus.OK) {
+      alert('got an okay status from geocoder');
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+};
