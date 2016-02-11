f704880fc2b4ebaa41bd5e28b000d2a50695a6be
alicekao <alicekao.ak@gmail.com>
2016-02-11 12:06:44 -0800

    (feat)Initial map

--- client/app/app.js
+++ client/app/app.js
@@ -1,7 +1,37 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 37.7833, lng: -122.4167},
+    zoom: 14
   });
+
+  // var geocoder = new google.mapsGeocoder();
+
+  var myLatLng = {lat: 37.7833, lng: -122.4167};
+  var contentString = "Goodbye World";
+
+  var infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+
+  var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: 'Hello World!'
+    });
+
+  marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
+
+  // document.getElementById('submit').addEventListener('click', function() {
+  //   geocodeAddress(geocoder, map);
+  // });
 }
+
+// function geocodeAddress(geocoder, resultsMap) {
+//   var address = document.getElementById('address').value;
+//   geocoder.geocode({'address': address}, function(results, status) {
+//     console.log('trying to geocode');
+//   });
+// }
