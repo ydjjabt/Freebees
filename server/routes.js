ae4d41ef939189ccd47700347f44da80488724ee
alicekao <alicekao.ak@gmail.com>
2016-02-11 16:12:40 -0800

    (feat)Added basic server routing to handle get/post reqs, but still need connection to db

---
+++ server/routes.js
@@ -0,0 +1,24 @@
+//need mongodb model here:
+//
+
+module.exports = function(app) {
+  //server routes
+  app.get('/', function(req, res) {
+    //use mongoose to get all items in db
+  });
+
+  //sample route for testing
+  app.get('/test', function(req, res) {
+    res.send('worked');
+  });
+
+  app.post('/', function(req, res) {
+    //insert item into mongodb
+  });
+
+  //any other route will load home page
+  app.get('*', function(req, res) {
+    res.sendFile('./client/index.html');
+    //or do we want to redirect to '/'?
+  });
+};
