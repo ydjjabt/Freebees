134f8f9803f80bbee3e4b162bed2eee3c70262fb
alicekao <alicekao.ak@gmail.com>
2016-02-11 16:42:34 -0800

    Added pseudocode documentation

--- client/app/app.js
+++ client/app/app.js
@@ -5,6 +5,7 @@ var initMap = function(){
     zoom: 14
   });
 
+  //Geocoder is an object Google maps w/ various methods API to pull their geocoding functionality
   var geocoder = new google.maps.Geocoder();
   //test data array
   var test = [
@@ -20,16 +21,19 @@ var initMap = function(){
 
   //loop through data returned from db to place on map
   for (var i = 0; i < test.length; i++) {
+    //create an instance of an info window that will show data when clicked
     var infowindow = new google.maps.InfoWindow({
         content: test[i].contentString
       });
-
+    
+    //create a new instance of a google maps marker, will be created for each item in our db
     var marker = new google.maps.Marker({
         position: test[i].myLatLng,
         map: map,
         title: 'Hello World!'
       });
-
+    
+    //create the click listener on each marker to show their respective info window
     marker.addListener('click', function() {
         infowindow.open(map, marker);
       });
@@ -40,9 +44,11 @@ var initMap = function(){
   });
 };
 
+//grab the address the client has typed in to send to turn into longitude/latitude
 var geocodeAddress = function(geocoder, resultsMap){
   var address = document.getElementById('address').value;
   alert('address is, ', address);
+  //calls the geocode method on Google Map's fancy object
   geocoder.geocode({'address': address}, function(results, status) {
     console.log('results from geocodeaddress fn is: ', results);
     if (status == google.maps.GeocoderStatus.OK) {
