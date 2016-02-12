e67aed384632fc4a192c46279c691e11b0a9243d
Jeehae Park <jpark2973@gmail.com>
2016-02-11 17:16:52 -0800

    (feat)Created mongo data base

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // express
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
 var app = express();
+
+//create mongo database
+mongoose.connet('mongodb://localhost/freebiesnearme');
+
 
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/client'));
 
 app.use(bodyParser.json());
+
+
 
 //routes
 require('./server/routes')(app);
