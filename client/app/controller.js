169d5e253964b80daaac7d7a706d430a05557d77
Collin Adams <collinadams04@gmail.com>
2016-02-16 13:15:32 -0800

    (feat) refined specific error console.logs to  requests in controller.js

--- client/app/controller.js
+++ client/app/controller.js
@@ -51,7 +51,7 @@ var app = angular.module('myApp', ['map.services'])
       //map within Map factory instead of global space.
 
     }, function(err){
-      console.log('error during saving row to db - failed to post to /: ', err);
+      console.log('Error when saveToDB invoked - post to "/" failed. Error: ', err);
     });
   };
 
@@ -71,7 +71,7 @@ var app = angular.module('myApp', ['map.services'])
         //re-initialize map with only these markers
         Map.initMap(filtered);
       }, function(err) {
-        console.log('error during filter - failed to get from /api/items', err);
+        console.log('Error when filterDB invoked - get from "/api/items" failed. Error: ', err);
       });
   };
 
@@ -83,7 +83,7 @@ var app = angular.module('myApp', ['map.services'])
         Map.removeMarker(map, data.data);
         //Map.loadAllItems();
       }, function(err) {
-        console.log('error during removal - failed to post to /pickup: ', err);
+        console.log('Error when removeFromDB invoked - post to "/pickup" failed. Error: ', err);
       });
   };
 
