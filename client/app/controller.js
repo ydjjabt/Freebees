e14067f6cc0fcb5a72b49cfc0f72fa91ed460716
Collin Adams <collinadams04@gmail.com>
2016-02-15 17:58:44 -0800

    (feat) cleaned pseudocode, deleted unnecessary console.logs, and unneccessary code

--- client/app/controller.js
+++ client/app/controller.js
@@ -2,21 +2,32 @@ var app = angular.module('myApp', ['map.services'])
 
 .controller('FormController', function($scope, $http, DBActions, Map){
   $scope.user = {};
+
   $scope.sendPost = function(){
     //convert inputted address
     Map.geocodeAddress(geocoder, Map.map, $scope.user.location, function(converted) {
+      //after address converted, save user input item and location to db
       DBActions.saveToDB({item: $scope.user.item, LatLng: converted});
     });
   };
+
+  //this function filters map based on what user enters into filter field
   $scope.filterMap = function() {
     var searchInput = $scope.search.input;
     DBActions.filterDB(searchInput);
   };
+
+  //this function retrieves everything from the database and renders a map on page
+  //this would happen when user first visits page, when user submits an item, or when user deletes an item
   $scope.initMap = function(){
     Map.loadAllItems();
   };
 })
+
 .factory('DBActions', function($http, Map){
+
+  //the 'toSave' parameter is an object that will be entered into database,
+  //it has item prop and LatLng properties
   var saveToDB = function(toSave){
   return $http.post('/', toSave)
     .then(function(data){
