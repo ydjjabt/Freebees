9ec9ce4f191661558435f9b0ef2b1f87c999fed1
Collin Adams <collinadams04@gmail.com>
2016-02-15 17:21:50 -0800

    cleaned pseudocode for indexModel

--- server/items/itemModel.js
+++ server/items/itemModel.js
@@ -1,12 +1,15 @@
 var mongoose = require('mongoose');
 
-
+//create a new mongoDB table called ItemSchema
 var ItemSchema = new mongoose.Schema ({
+
+  //ItemSchema table has a column for itemName, which is a string
   itemName :{
     type: String,
     required: true
   },
 
+  //ItemSchema table has a column for itemLocation, which is an object with lat and lng props
   itemLocation :{
     type: Object,
     required: true
@@ -14,5 +17,6 @@ var ItemSchema = new mongoose.Schema ({
 
 });
 
+//export the Item mongoose model that will fit into the ItemSchema table
 module.exports = mongoose.model('Item', ItemSchema);
 
