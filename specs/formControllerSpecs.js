2de200c52bdb28f9ddf208561ea432c009df125f
Collin Adams <collinadams04@gmail.com>
2016-02-17 12:32:08 -0800

    (test)Created spec file for formController

---
+++ specs/formControllerSpecs.js
@@ -0,0 +1,24 @@
+"use strict";
+
+describe('FormController', function(){
+  var $scope, $rootScope, $http, createController, DBActions, Map;
+
+  beforeEach(module('myApp'));
+
+  beforeEach(inject(function($injector){
+    $rootScope = $injector.get('$rootScope');
+    DBActions = $injector.get('DBActions');
+    Map = $injector.get('Map');
+    $scope = $rootScope.$new();
+
+    var $controller = $injector.get('$controller');
+
+    createController = function(){
+      return $controller('FormController', {
+        $scope: $scope,
+        DBActions: DBActions,
+        Map: Map
+      });
+    }
+  }))
+})
