4fd6824ff10059dc9fb2c1ee5b4850dfd83a2976
Zak Golding <hello@zakarhino.com>
2016-02-27 14:31:30 -0800

    Started work on Factory conversion

--- client/app/controller.js
+++ client/app/controller.js
@@ -99,8 +99,6 @@ var app = angular.module('myApp', ['map.services', 'auth'])
     $scope.clearForm();
   };
 
-  $scope.log
-
   //fills in the address field with current lat/lng
   $scope.ip = function(){
     startSpinner();
@@ -119,93 +117,4 @@ var app = angular.module('myApp', ['map.services', 'auth'])
     }
   };
   $scope.clearForm();
-})
-
-.factory('DBActions', function($http, $window, Map){
-  //the 'toSave' parameter is an object that will be entered into database,
-  //'toSave' has item prop and LatLng properties
-  var addUser = function (user){
-    return $http.post('/user', user)
-    .then (function (data){
-      console.log(data);
-
-    }, function(err){
-      console.log("error when adduser invoked - post to users failed.Error", err);
-    });
-  }
-
-  var saveToDB = function(toSave){
-  return $http({
-    method: 'POST',
-    url: '/submit',
-    headers: {
-      'x-access-token': $window.localStorage.token
-    },
-    data: toSave
-  })
-    //after item has been saved to db, returned data has a data property
-    //so we need to access data.data, see below
-    .then(function(data){
-      stopSpinner();
-      //data.data has itemName prop, itemLocation prop, and _id prop, which are all expected since this is how
-      //our mongoDB is formatted. Anything returned from db should have these props
-      Map.addMarker(map, data.data, infoWindow);
-      //the 'map' argument here is referencing the global map declared in app.js
-      //this could be manipulated in chrome console by user. Future refactor could be to store
-      //map within Map factory instead of global space.
-    }, function(err){
-      console.log('Error when saveToDB invoked - post to "/" failed. Error: ', err);
-    });
-  };
-
-  //this function creates a new map based on filtering by whatever user enters in filter field
-  //it is invoked within $scope.filterMap, see the above controller
-  var filterDB = function(toFilterBy){
-
-    //gets everything from the db in an obj referenced as data
-    return $http({
-      method: 'GET',
-      url: '/api/items',
-      headers: {
-        'x-access-token': $window.localStorage.token
-      },
-    })
-      .then(function(data){
-        //console.log(data)
-        //filter our returned db by the desired itemName
-        var filtered = data.data.filter(function(item){
-          return item.itemName.indexOf(toFilterBy) > -1;
-        });
-
-        //re-initialize map with only these markers
-        Map.initMap(filtered);
-      }, function(err){
-        console.log('Error when filterDB invoked - get from "/api/items" failed. Error: ', err);
-      });
-  };
-
-  var removeFromDB = function(toRemove){
-    console.log("Called removeFromDB");
-    return $http({
-      method: 'POST',
-      url: '/api/items',
-      headers: {
-        'x-access-token': $window.localStorage.token
-      },
-      data: toRemove
-    })
-      .then(function(data){
-        loadAllItems();
-      }, function(err){
-        console.log('Error when removeFromDB invoked - post to "/pickup" failed. Error: ', err);
-      });
-  };
-
-  //the DBActions factory returns the below object with methods of the functions
-  //defined above
-  return {
-    saveToDB: saveToDB,
-    filterDB: filterDB,
-    removeFromDB: removeFromDB
-  };
 })
